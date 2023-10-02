e643069cd99fd89714ecfcd1a6beb4eb399f1a3e
Azmi Touil <atouil@exoplatform.com>
2023-10-02 16:19:38 +0200

    Update PR

--- gamification-twitter-webapp/webpack.watch.js
+++ gamification-twitter-webapp/webpack.watch.js
@@ -3,7 +3,7 @@ const { merge } = require('webpack-merge');
 const webpackCommonConfig = require('./webpack.prod.js');
 
 // the display name of the war
-const app = 'gamification-twitter';
+const app = 'gamification-twitter-webapp';
 
 const exoServerPath = "/exo-server";
 
