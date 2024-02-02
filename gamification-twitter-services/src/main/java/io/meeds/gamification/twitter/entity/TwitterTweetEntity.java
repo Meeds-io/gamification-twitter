9a7357cc1144aaf0f6b1c834ce4bc1fb6c083e7b
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-02-02 11:17:49 +0100

    feat: Add like tweet and retweet events for twitter - MEED-2967 - Meeds-io/MIPs#105 (#56)
    This PR will implement 2 news events (like and retweet) for the twitter connector

---
+++ gamification-twitter-services/src/main/java/io/meeds/gamification/twitter/entity/TwitterTweetEntity.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.gamification.twitter.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity(name = "TwitterTweets")
+@Table(name = "TWITTER_TWEETS")
+
+@NamedQuery(name = "TwitterTweets.getTweetByLink",
+        query = "SELECT tweet FROM TwitterTweets tweet"
+                + " WHERE tweet.tweetLink = :tweetLink")
+@NamedQuery(name = "TwitterTweets.getTweetsIds",
+        query = "SELECT tweet.tweetId FROM TwitterTweets tweet"
+                + " ORDER BY tweet.tweetId ASC")
+
+@Data
+public class TwitterTweetEntity implements Serializable {
+
+    private static final long serialVersionUID = -4871930064565777769L;
+
+    @Id
+    @SequenceGenerator(name = "SEQ_TWITTER_ACCOUNTS_ID", sequenceName = "SEQ_TWITTER_ACCOUNTS_ID", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_TWITTER_ACCOUNTS_ID")
+    @Column(name = "TWEET_ID")
+    private Long   tweetId;
+
+    @Column(name = "TWEET_LINK")
+    private String   tweetLink;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "TWITTER_TWEET_LIKERS", joinColumns = @JoinColumn(name = "TWEET_ID"))
+    @Column(name = "LIKER_USERNAME")
+    private Set<String> likers;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "TWITTER_TWEET_RETWEETERS", joinColumns = @JoinColumn(name = "TWEET_ID"))
+    @Column(name = "RETWEETER_USERNAME")
+    private Set<String> retweeters;
+}
--- gamification-twitter-webapp/src/main/webapp/vue-app/engagementCenterExtensions/extensions.js
+++ gamification-twitter-webapp/src/main/webapp/vue-app/engagementCenterExtensions/extensions.js
@@ -23,10 +23,9 @@ export function init() {
       rank: 60,
       image: '/gamification-twitter/images/TwitterX.svg',
       match: (actionLabel) => [
-        'followAccount',
         'likeTweet',
         'mentionAccount',
-        'repostTweet',
+        'retweet',
       ].includes(actionLabel),
       getLink: realization => {
         if (realization.objectType === 'tweet' && realization.objectId > 0) {
@@ -34,6 +33,7 @@ export function init() {
           return realization.link;
         }
       },
+      isExtensible: true
     }
   });
 }
