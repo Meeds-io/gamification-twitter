25098e5bf2159427bffb6ed3ef224f2ded2b1cb4
Boubaker Khanfir <bkhanfir@exoplatform.com>
2024-06-21 16:49:59 +0100

    fix: Avoid using v-app in Vue components not main DOM of an app - MEED-7094 - Meeds-io/MIPs#144 (#79) (#81)
    Prior to this change, the v-app was used in Vue components injected via extensionRegistry. This change will change it to simply use a simple div.

--- gamification-twitter-webapp/src/main/webapp/vue-app/twitterAdminConnectorExtension/components/TwitterAdminConnectorItem.vue
+++ gamification-twitter-webapp/src/main/webapp/vue-app/twitterAdminConnectorExtension/components/TwitterAdminConnectorItem.vue
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
-  <v-app>
+  <div>
     <template v-if="!displayAccountDetail">
       <div class="px-4 py-2 py-sm-5 d-flex align-center">
         <v-tooltip :disabled="$root.isMobile" bottom>
@@ -124,7 +124,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
       :ok-label="$t('confirm.yes')"
       :cancel-label="$t('confirm.no')"
       @ok="deleteSettings" />
-  </v-app>
+  </div>
 </template>
 
 <script>
